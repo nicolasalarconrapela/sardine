7cb2da4a167caf4fb4c9a25dc094bb6c0e5f74f3
nicolasalarconrapela <nicolasalarconrapela@gmail.com>
2020-02-13 11:50:47 +0100

    Sard : Upd. V.0.0.1D(130220115043)

--- src/test/java/com/github/sardine/impl/SardineExceptionTest.java
+++ src/test/java/com/github/sardine/impl/SardineExceptionTest.java
@@ -10,8 +10,9 @@ public class SardineExceptionTest
 	@Test
 	public void testMessage()
 	{
+		// TODO : REVIEW : Upd a V.0.0.1D
 		final SardineException e = new SardineException("m", 400, "response phrase");
-		assertEquals("m (400 response phrase)", e.getMessage());
+		assertEquals("status code: 400, reason phrase: m (400 response phrase)", e.getMessage());
 		assertEquals("response phrase", e.getResponsePhrase());
 		assertEquals(400, e.getStatusCode());
 	}
